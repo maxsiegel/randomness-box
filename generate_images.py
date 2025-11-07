f810954528256ac9ea83fe5daffc90ba2c3327db
Max Siegel <maxs@mit.edu>
2025-11-07 16:27:52 -0500

    _

--- generate_images.py
+++ generate_images.py
@@ -1,18 +1,22 @@
 from itertools import product
 from os.path import join
 
+
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+
 
 def draw_empty_reminder():
     fig, ax = plt.subplots(figsize=(8, 2))
     ax.set_xlim(0, 10)
     ax.set_ylim(0, 2)
-    ax.set_aspect('equal')
-    ax.axis('off')
+    ax.set_aspect("equal")
+    ax.axis("off")
 
     # Background rectangle
-    background = patches.Rectangle((0, 0), 10, 2, linewidth=2, edgecolor='black', facecolor='#d2a679')
+    background = patches.Rectangle(
+        (0, 0), 10, 2, linewidth=2, edgecolor="black", facecolor="#d2a679"
+    )
     ax.add_patch(background)
 
     plt.savefig(join("images", "background_only.png"))
@@ -31,30 +35,39 @@ def draw_reminder(colors):
     fig, ax = plt.subplots(figsize=(8, 2))
     ax.set_xlim(0, 10)
     ax.set_ylim(0, 2)
-    ax.set_aspect('equal')
-    ax.axis('off')
+    ax.set_aspect("equal")
+    ax.axis("off")
 
     # Background rectangle
-    background = patches.Rectangle((0, 0), 10, 2, linewidth=2,
-                                   edgecolor='black', facecolor='#d2a679')
+    background = patches.Rectangle(
+        (0, 0), 10, 2, linewidth=2, edgecolor="black", facecolor="#d2a679"
+    )
     ax.add_patch(background)
 
     # Draw N boxes
     for i in range(N):
         x = 1.5 + i * 1.8
-        rect = patches.FancyBboxPatch((x-0.6, 0.4), 1.2, 1.2,
-                                      boxstyle="round,pad=0.1,rounding_size=0.2",
-                                      linewidth=2, edgecolor="black", facecolor="#f4d7b8")
+        rect = patches.FancyBboxPatch(
+            (x - 0.6, 0.4),
+            1.2,
+            1.2,
+            boxstyle="round,pad=0.1,rounding_size=0.2",
+            linewidth=2,
+            edgecolor="black",
+            facecolor="#f4d7b8",
+        )
         ax.add_patch(rect)
 
         # Only draw circles for provided colors
         if i < N:
-            circle = patches.Circle((x, 1.0), 0.45,
-                                    facecolor=colors[i], edgecolor="black")
+            circle = patches.Circle(
+                (x, 1.0), 0.45, facecolor=colors[i], edgecolor="black"
+            )
             ax.add_patch(circle)
 
     plt.savefig(join("images", "_".join(colors) + ".png"))
     plt.close()
+
 
 def draw_single_circle(color, radius=0.5):
     """
@@ -67,20 +80,23 @@ def draw_single_circle(color, radius=0.5):
     fig, ax = plt.subplots(figsize=(2, 2))
     ax.set_xlim(-1, 1)
     ax.set_ylim(-1, 1)
-    ax.set_aspect('equal')
-    ax.axis('off')
+    ax.set_aspect("equal")
+    ax.axis("off")
 
     circle = patches.Circle((0, 0), radius, facecolor=color, edgecolor="black")
     ax.add_patch(circle)
 
     plt.savefig(join("images", "one_" + color + "_marble.png"))
 
+
 def generate_all_reminder_images():
     NUM_TRIALS = 5
-    contrasts = [("blue", "yellow"),
-                ("green", "red"),
-                ("red", "pink"),
-                ("orange", "purple")]
+    contrasts = [
+        ("blue", "yellow"),
+        ("green", "red"),
+        ("red", "pink"),
+        ("orange", "purple"),
+    ]
 
     for contrast in contrasts:
         for length in range(1, NUM_TRIALS + 1):
@@ -89,13 +105,13 @@ def generate_all_reminder_images():
             for seq in spec:
                 draw_reminder(seq)
 
+
 def generate_single_marble_images():
-    colors = ["blue", "yellow",
-              "green", "red",
-              "red", "pink",
-              "orange", "purple"]
+    colors = ["blue",
+              "yellow", "green", "red", "red", "pink", "orange", "purple"]
 
     for color in colors:
         draw_single_circle(color)
 
-# generate_single_marble_images()
+
+generate_single_marble_images()
